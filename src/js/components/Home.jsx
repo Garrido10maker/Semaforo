0d3f44a7ce30d0875a628eac8802304dffa24ae9
Garrido10maker <santiagophd2808@gmail.com>
2026-05-09 02:38:14 +0000

    Semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,24 +5,41 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const colors = {
+	"red"
+	"yellow" 
+	"green"
+	}
+
+	 function isColorActive ){
+		
+		if (color == "red"({
+		return "bg-danger"
+		}
+		if (color == "yellow") {
+		}
+		if (color) == "green") {
+			return "bg-success"
+		}
+	}
+
+	function isColorActive (color) {
+	if (color == "red"){
+	return "turn-on-red"
+	}
+	if (color == "yellow") {
+	return "turn-on-yellow
+	}
+	if (color == "green"){
+		return "turn-on-green"
+	}
+	}
 	return (
-		<div className="text-center">
-            
+		<div className="d-flex flex-column align-items-center">
+		<div className="bg-dark p-2" style={{width: '5px', height: '40px'}}></div>
+		<div className="d-inline-flex bg-dark">
+		<div className="d-flex flex-column align-items-center gap-2 p-2">
+			{)
+	}
+	</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
